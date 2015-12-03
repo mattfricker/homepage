a44fe682febf8771c8a8df8364c629d640f55c30
Matt <matt.s.fricker@gmail.com>
2015-12-02 23:34:07 -0500

    update the server.js to remove unnecessary items

--- server.js
+++ server.js
@@ -2,17 +2,9 @@
 
 var express = require('express'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser'),
 	env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
 	app = express(),
 	port = 3030,
-	mysql   = require('mysql'),
-    connectionpool = mysql.createPool({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'portfolio'
-    });
 
 app.set('views', __dirname + '/src/client/');
 app.set('view engine', 'jade');
