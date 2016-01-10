48ca37bcea5a542b522a27bf36f0de25e5bbe0d3
Matt <matt.s.fricker@gmail.com>
2016-01-10 08:25:19 -0500

    draw portfolio from view model and change to ng-repeat

--- precompiledJS/app/home/homeController.js
+++ precompiledJS/app/home/homeController.js
@@ -11,7 +11,23 @@
     	var vm = this;
 		vm.redirect = function redirect (newLocation) {
 			window.location = newLocation;
-		}
+		};
+		vm.portfolio = [
+			{
+				title: 'Center for the Study of Targeted Killing',
+				link: 'http://targetedkilling.org/#/',
+				imageSrc: '/img/cstk.png',
+				imageAlt: 'screenshot of CSTK homepage',
+				tools: ['Angular', 'Express', 'Git', 'Sass', 'Gulp', 'Bower', 'Css', 'HTML', 'JavaScript', 'JSON', 'Babel', 'npm', 'Node', 'MySQL']
+			}, 
+			{
+				title: 'My Homepage (version four)',
+				link: '/#/',
+				imageSrc: '/img/homepage.png',
+				imageAlt: 'screenshot of the fourth version of my homepage',
+				tools: ['Angular', 'Express', 'Git', 'Sass', 'Gulp', 'Bower', 'Css', 'HTML', 'JavaScript', 'JSON', 'Babel', 'npm', 'Node', 'Mongo']
+			}
+		];
     }
 
 })();
