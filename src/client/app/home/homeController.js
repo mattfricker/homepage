41dd3b7de8a4379ef0cedcb4ca754744265e95b5
Matt <matt.s.fricker@gmail.com>
2016-01-09 22:36:14 -0500

    add link to blog list

--- src/client/app/home/homeController.js
+++ src/client/app/home/homeController.js
@@ -9,10 +9,6 @@
 
 	function HomeController() {
 		var vm = this;
-		vm.blogText = "Blog";
-		vm.comingSoon = function comingSoon() {
-			vm.blogText = "Coming soon";
-		};
 		vm.redirect = function redirect(newLocation) {
 			window.location = newLocation;
 		};
