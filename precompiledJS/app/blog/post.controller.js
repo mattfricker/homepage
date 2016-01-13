8b75033b99bc0cae8339f4be4f2e9ec38dfcb08a
Matt <matt.s.fricker@gmail.com>
2016-01-13 16:54:14 -0500

    update date formatting in the app, as teh database now stores it in correct format

--- precompiledJS/app/blog/post.controller.js
+++ precompiledJS/app/blog/post.controller.js
@@ -13,8 +13,6 @@
 		vm.post;
 		BlogPostsService.getBlogPost()
 			.then(data => vm.post = data[0])
-			//REMOVE SUBSEQUENT THEN ONCE DATABASE STORES DATE FORMAT CORRECTLY
-			.then(() => vm.post.date = new Date(vm.post.date))
     }
 
 })();
--- src/client/app/blog/post.controller.js
+++ src/client/app/blog/post.controller.js
@@ -13,10 +13,6 @@
 		vm.post;
 		BlogPostsService.getBlogPost().then(function (data) {
 			return vm.post = data[0];
-		})
-		//REMOVE SUBSEQUENT THEN ONCE DATABASE STORES DATE FORMAT CORRECTLY
-		.then(function () {
-			return vm.post.date = new Date(vm.post.date);
 		});
 	}
 })();
